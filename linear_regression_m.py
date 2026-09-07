5027268817bd0572d0af119a401e2bd0d11b2b4a
Davi Dias Vale <davidiasvalept@gmail.com>
2026-09-07 13:53:06 -0300

    adicionando MSE, MAE, RSS, e uma nova class
    adicionando MSE, MAE, RSS, e uma nova classe, a nova classe serve apenas para a f) da segundo (e provavelmente pra g) da primeira, ela força o intercepto ser 0

--- linear_regression_m.py
+++ linear_regression_m.py
@@ -39,4 +39,47 @@ class MRegression:
 
         return np.sqrt(media)
 
-    
+    #####
+    def mse(self, y_true, y_pred):
+        erro = (y_true - y_pred) ** 2
+
+        return np.mean(erro)
+
+    def mae(self, y_true, y_pred):
+        erro = np.abs(y_true - y_pred)
+
+        return np.mean(erro)
+
+    def rss(self, y_true, y_pred):
+        erro = y_true - y_pred
+
+        return np.sum(erro ** 2)
+
+#é para o letra f) da q2, mas acho q serve pra g) da q1
+class MRegressionZeroIntercept:
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+        self.beta = None
+        self.N = X.shape[0]
+
+    def fit(self):
+        self.beta = np.linalg.pinv(self.X.T @ self.X) @ self.X.T @ self.y
+
+        return self
+
+    def predict(self, X_new):
+        return X_new @ self.beta
+
+    def r2_score(self, y_true, y_preditivo):
+        numerador = np.sum((y_true - y_preditivo) ** 2)
+        denominador = np.sum((y_true - np.mean(y_true)) ** 2)
+
+        r2_score = 1 - (numerador / denominador)
+
+        return r2_score
+
+    def rmse(self, y_true, y_pred):
+        erro = (y_true - y_pred) ** 2
+
+        return np.sqrt(np.mean(erro))
